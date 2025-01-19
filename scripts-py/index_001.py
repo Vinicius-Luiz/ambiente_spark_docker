c6fe59e53c069ee4f5a2313a21d128de584cf849
= <=>
2025-01-18 23:10:12 -0300

    ADD Log Erros: Em andamento erro de salvar arquivo

--- scripts-py/index_001.py
+++ scripts-py/index_001.py
@@ -6,32 +6,32 @@ findspark.init()
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-print("Inicia a SparkSession")
-# Criação da SparkSession com a configuração necessária
+print(" ===//===//===//===//===//===//>> Inicia a SparkSession")
 spark = SparkSession.builder\
     .appName(f"Filtragem de dados - {timestamp}")\
     .config("spark.hadoop.security.authentication", "simple")\
-    .getOrCreate()  # Criação da SparkSession e atribuição à variável 'spark'
+    .config("spark.hadoop.security.authorization", "false")\
+    .getOrCreate()
 
-print("Cria um DataFrame simples com alguns dados")
+print(" ===//===//===//===//===//===//>> Cria um DataFrame simples com alguns dados")
+columns = ["id", "nome", "idade"]
 data = [
     (1, "Vinicius", 23),
     (2, "Allana", 21),
     (3, "Vital", 22),
     (4, "Gabriela", 25)
 ]
-columns = ["id", "nome", "idade"]
 
 df = spark.createDataFrame(data, columns)
 
-print("Filtra o DataFrame (idade menor que 23)")
+print(" ===//===//===//===//===//===//>> Filtra o DataFrame (idade menor que 23)")
 filtered_df = df.filter(df.idade < 23)
 
-print("Salva o DataFrame filtrado em um arquivo de texto")
-output_path = "/opt/bitnami/spark/conf/output/filtered_data.txt"
-filtered_df.write.mode("overwrite").text(output_path)
+print(" ===//===//===//===//===//===//>> Salva o DataFrame filtrado em um arquivo de CSV")
+output_path = "/opt/bitnami/spark/conf/output/filtered_data.csv"
+filtered_df.write.csv(output_path, header=True)
 
 filtered_df.show(truncate=False)
 
-print("Encerra a SparkSession")
+print(" ===//===//===//===//===//===//>> Encerra a SparkSession")
 spark.stop()
